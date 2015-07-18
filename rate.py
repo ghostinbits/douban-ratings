a1ab0f525b94c541ec85ff0075b649d4ad852881
Jackie Tsui <vexetron@gmail.com>
2015-07-18 11:49:19 +0800

    Create rate.py

---
+++ rate.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import re
+import sys
+import urllib.parse
+import urllib.request
+
+
+class Movie:
+
+    def __init__(self, name):
+        self.name = name
+
+    def douban_rate(self):
+        query = urllib.parse.urlencode([('cat', '1002'), ('q', name)])
+        url = 'http://www.douban.com/search?{}'.format(query)
+        response = urllib.request.urlopen(url).read().decode()
+        rate = re.findall(r'(?<=<span class="rating_nums">)\d\.\d',
+                          response)
+        cast = re.findall(r'(?<=<span class="subject-cast">).*(?=</span>)',
+                          response)
+        return zip(cast, rate)
+
+
+if __name__ == '__main__':
+    while True:
+        try:
+            name = sys.argv[1]
+        except:
+            name = input('Input a movie name: ')
+        movie = Movie(name)
+        for i in movie.douban_rate():
+            print(' / '.join(i))
